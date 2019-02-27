2889fd1a48af3d0bc328eca21717b5b108a6e86e
I_Jemin <i_jemin@hotmail.com>
2019-02-27 16:51:32 +0900

    Add BubbleSort

---
+++ BubbleSort.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+					
+public class Program
+{
+	public static void Main()
+	{
+		Action<int[]> bubleSort = (int[] list) => {
+			bool again = false;
+			int limit = list.Length;
+			
+			do {
+				again = false;
+				
+				for(var i = 0; i < limit; i++) {
+					var currentValue = list[i];
+					var nextValue = i + 1 < limit ? list[i+1] : int.MaxValue;
+
+					if(nextValue < currentValue) {
+						list[i] = nextValue;
+						list[i+1] = currentValue;
+						again = true;
+					}
+				}
+			} while(again);
+		};
+		
+		int[] randomList = new int[] {23,1,5,16,42,15,4,16,8};
+		bubleSort(randomList);
+		
+		Console.WriteLine(string.Join(", ", randomList));
+	}
+}
