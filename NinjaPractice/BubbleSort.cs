c688e053f4e3bad5365e6f9fc8b6ea6ec691d5b1
I_Jemin <i_jemin@hotmail.com>
2019-03-28 20:09:56 +0900

    Fix Class names

--- NinjaPractice/BubbleSort.cs
+++ NinjaPractice/BubbleSort.cs
@@ -1,33 +1,37 @@
 using System;
 using System.Collections;
-					
-public class Program
-{
-	public static void Main()
-	{
-		Action<int[]> bubleSort = (int[] list) => {
-			bool again = false;
-			int limit = list.Length;
-			
-			do {
-				again = false;
-				
-				for(var i = 0; i < limit; i++) {
-					var currentValue = list[i];
-					var nextValue = i + 1 < limit ? list[i+1] : int.MaxValue;
 
-					if(nextValue < currentValue) {
-						list[i] = nextValue;
-						list[i+1] = currentValue;
-						again = true;
-					}
-				}
-			} while(again);
-		};
-		
-		int[] randomList = new int[] {23,1,5,16,42,15,4,16,8};
-		bubleSort(randomList);
-		
-		Console.WriteLine(string.Join(", ", randomList));
-	}
+public class BubbleSort
+{
+    public void Run()
+    {
+        Action<int[]> bubleSort = (int[] list) =>
+        {
+            bool again = false;
+            int limit = list.Length;
+
+            do
+            {
+                again = false;
+
+                for (var i = 0; i < limit; i++)
+                {
+                    var currentValue = list[i];
+                    var nextValue = i + 1 < limit ? list[i + 1] : int.MaxValue;
+
+                    if (nextValue < currentValue)
+                    {
+                        list[i] = nextValue;
+                        list[i + 1] = currentValue;
+                        again = true;
+                    }
+                }
+            } while (again);
+        };
+
+        int[] randomList = new int[] {23, 1, 5, 16, 42, 15, 4, 16, 8};
+        bubleSort(randomList);
+
+        Console.WriteLine(string.Join(", ", randomList));
+    }
 }
--- NinjaPractice/QuickSort.cs
+++ NinjaPractice/QuickSort.cs
@@ -1,40 +1,44 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
-					
-public class Program
+
+public class QuickSortBundle
 {
-	public static void Main()
-	{
-		List<int> randomList = new List<int>(new int[] {23,1,5,16,42,15,4,16,8,3});	
-		List<int> sortedList = QuickSort(randomList);
-		
-		Console.WriteLine(string.Join(", ", sortedList));
-	}
-	
-	private static List<int> QuickSort(List<int> list) {
-		if(list.Count < 2) return list;
-		
-		var pivotIndex = list.Count - 1;
-		var pivotValue = list[pivotIndex];
-		list.RemoveAt(pivotIndex);
-		
-		var left = new List<int>();
-		var right = new List<int>();
-		
-		for(var i = 0; i < list.Count; i++) {
-			if(list[i] < pivotValue) {
-				left.Add(list[i]);	
-			} else {
-				right.Add(list[i]);
-			}
-		}
-		
-		List<int> result;
-		result = QuickSort(left);
-		result.Add(pivotValue);
-		result.AddRange(QuickSort(right));
-		return result;
-	}
-	
+    public void Run()
+    {
+        List<int> randomList = new List<int>(new int[] {23, 1, 5, 16, 42, 15, 4, 16, 8, 3});
+        List<int> sortedList = QuickSort(randomList);
+
+        Console.WriteLine(string.Join(", ", sortedList));
+    }
+
+    private List<int> QuickSort(List<int> list)
+    {
+        if (list.Count < 2) return list;
+
+        var pivotIndex = list.Count - 1;
+        var pivotValue = list[pivotIndex];
+        list.RemoveAt(pivotIndex);
+
+        var left = new List<int>();
+        var right = new List<int>();
+
+        for (var i = 0; i < list.Count; i++)
+        {
+            if (list[i] < pivotValue)
+            {
+                left.Add(list[i]);
+            }
+            else
+            {
+                right.Add(list[i]);
+            }
+        }
+
+        List<int> result;
+        result = QuickSort(left);
+        result.Add(pivotValue);
+        result.AddRange(QuickSort(right));
+        return result;
+    }
 }
